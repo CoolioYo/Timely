f866b9035da2c2452c8357bed80b44f3ded7d2a5
CoolioYo <georgiacmartinez@gmail.com>
2020-07-07 04:23:51 -0500

    Started CSS for website list

--- popup.js
+++ popup.js
@@ -6,29 +6,58 @@ document.addEventListener("DOMContentLoaded", function() {
     // Add a website to track
     addWebsite.addEventListener("click", function() {
         // Parse URL
-        var websiteInput = document.getElementById("website-input").value;
-        websiteInput = new URL(websiteInput).host;
+        var url = document.getElementById("search-bar").value;
+        url = new URL(url).host;
 
         // Check if the website is already being tracked
-        if(!websites.includes(websiteInput)){
-            var newWebsite = document.createElement("p");
-            var websiteText = document.createTextNode(websiteInput);
-            newWebsite.appendChild(websiteText);
+        if(!websites.includes(url)){
+            var container = document.createElement("div"); // Div for website elements
+            container.classList.add("flex");
+
+            var favicon = document.createElement("img"); // Favicon
+            favicon.src = "https://www.google.com/s2/favicons?domain="+url;
+
+            var name = document.createElement("p"); // Website name
+            name.appendChild(document.createTextNode(url));
+            name.id = "website-name";
+
+            var remove = document.createElement("button"); // Remove button
+            remove.appendChild(document.createTextNode("x"));
+            remove.id = "remove-website";
+
+            container.appendChild(favicon);
+            container.appendChild(name);
+            container.appendChild(remove);
+
+            // Remove website from tracking list
+            remove.addEventListener("click", function () {
+                var toBeRemoved = this.parentElement.children[1].textContent;
+
+                for(var i = 0; i < websites.length; i++){
+                    if(websites[i] == toBeRemoved){
+                        websites.splice(i, 1);
+                    }
+                }
+
+                this.parentNode.remove();
+            });
     
             var websitesTracked = document.getElementById("websites-tracked");
-            websitesTracked.appendChild(newWebsite);
+            websitesTracked.appendChild(container);
             
-            websites.push(websiteInput);
+            websites.push(url);
 
+            // Send URL to background.js
             chrome.runtime.sendMessage({
-                msg: websiteInput
+                msg: url
             });
 
-            console.log(websiteInput + " was added");
-            document.getElementById("website-input-error").innerHTML = ""; 
+            console.log(url + " was added");
+            document.getElementById("input-error").innerHTML = ""; 
 
         }else{
-            document.getElementById("website-input-error").innerHTML = "You are already tracking this site";
+            // If the website is already being tracked
+            document.getElementById("input-error").innerHTML = "You are already tracking this site";
         }
     });
 });
